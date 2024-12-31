1d2e80bbd8863c6b6188418b78f9802e4f069967
Anirudh Kamath <github@kamath.io>
2024-12-30 19:58:25 -0500

    add config

---
+++ stagehand-quickstart/blank.ts
@@ -0,0 +1,49 @@
+/**
+ * 🤘 Welcome to Stagehand!
+ *
+ * TO RUN THIS PROJECT:
+ * ```
+ * npm install
+ * npm run start
+ * ```
+ *
+ * To edit config, see `stagehand.config.ts`
+ *
+ */
+
+import StagehandConfig from "./stagehand.config.ts";
+import { Page, BrowserContext, Stagehand } from "@browserbasehq/stagehand";
+import { z } from "zod";
+import chalk from "chalk";
+import boxen from "boxen";
+import dotenv from "dotenv";
+import { announce } from "./utils.ts";
+
+dotenv.config();
+
+async function main({
+  page,
+  context,
+  stagehand,
+}: {
+  page: Page; // Playwright Page with act, extract, and observe methods
+  context: BrowserContext; // Playwright BrowserContext
+  stagehand: Stagehand; // Stagehand instance
+}) {
+  // Add your code here
+}
+
+(async () => {
+  const stagehand = new Stagehand({
+    ...StagehandConfig,
+  });
+  await stagehand.init();
+  const page = stagehand.page;
+  const context = stagehand.context;
+  await main({
+    page,
+    context,
+    stagehand,
+  });
+  await stagehand.close();
+})().catch(console.error);
