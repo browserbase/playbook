9352a43f10e4f4f83dee18744da3aba28bf25bf2
Anirudh Kamath <github@kamath.io>
2025-02-05 20:26:25 -0800

    new quickstart for v1.11

--- examples/quickstart.ts
+++ examples/quickstart.ts
@@ -9,19 +9,12 @@
  *
  * To edit config, see `stagehand.config.ts`
  *
- * In this quickstart, we'll be automating a browser session to show you the power of Playwright and Stagehand's AI features.
- *
- * 1. Go to https://docs.browserbase.com/
- * 2. Use `extract` to find information about the quickstart
- * 3. Use `observe` to find the links under the 'Guides' section
- * 4. Use Playwright to click the first link. If it fails, use `act` to gracefully fallback to Stagehand AI.
  */
-
 import { Page, BrowserContext, Stagehand } from "@browserbasehq/stagehand";
 import { z } from "zod";
 import chalk from "chalk";
-import boxen from "boxen";
 import dotenv from "dotenv";
+import { clearOverlays, drawObserveOverlay } from "./utils.js";
 
 dotenv.config();
 
@@ -34,141 +27,48 @@ export async function main({
   context: BrowserContext; // Playwright BrowserContext
   stagehand: Stagehand; // Stagehand instance
 }) {
-  console.log(
-    [
-      `🤘 ${chalk.yellow("Welcome to Stagehand!")}`,
-      "",
-      "Stagehand is a tool that allows you to automate browser interactions.",
-      "Watch as this demo automatically performs the following steps:",
-      "",
-      `📍 Step 1: Stagehand will auto-navigate to ${chalk.blue(
-        "https://docs.browserbase.com/"
-      )}`,
-      `📍 Step 2: Stagehand will use AI to ${chalk.green(
-        "extract"
-      )} information about the quickstart`,
-      `📍 Step 3: Stagehand will use AI to ${chalk.green(
-        "observe"
-      )} and identify links in the 'Guides' section`,
-      `📍 Step 4: Stagehand will attempt to click the first link using Playwright, with ${chalk.green(
-        "act"
-      )} as an AI fallback`,
-    ].join("\n")
-  );
-
-  //   You can use the `page` instance to write any Playwright code
-  //   For more info: https://playwright.dev/docs/pom
-  await page.goto("https://docs.browserbase.com/");
-
-  const description = await page.extract({
-    instruction: "extract the title, description, and link of the quickstart",
-    // Zod is a schema validation library similar to Pydantic in Python
-    // For more information on Zod, visit: https://zod.dev/
-    schema: z.object({
-      title: z.string(),
-      link: z.string(),
-      description: z.string(),
-    }),
-  });
-  announce(
-    `The ${chalk.bgYellow(description.title)} is at: ${chalk.bgYellow(
-      chalk.blue(description.link)
-    )}` +
-      `\n\n${chalk.bgYellow(description.description)}` +
-      `\n\n${chalk.gray(JSON.stringify(description, null, 2))}`,
-    "Extract"
-  );
-
-  const observeResult = await page.observe({
-    instruction: "Find the links under the 'Guides' section",
-  });
-  announce(
-    `${chalk.green("Observe:")} We can click:\n${observeResult
-      .map(
-        (r) => `"${chalk.yellow(r.description)}" -> ${chalk.gray(r.selector)}`
-      )
-      .join("\n")}`,
-    "Observe"
-  );
-
-  //   In the event that your Playwright code fails, you can use the `act` method to
-  //   let Stagehand AI take over and complete the action.
-  try {
-    throw new Error(
-      "Comment out line 118 in index.ts to run the base Playwright code!"
-    );
-
-    // Wait for search button and click it
-    const quickStartSelector = `#content-area > div.relative.mt-8.prose.prose-gray.dark\:prose-invert > div > a:nth-child(1)`;
-    await page.waitForSelector(quickStartSelector);
-    await page.locator(quickStartSelector).click();
-    await page.waitForLoadState("networkidle");
-    announce(
-      `Clicked the quickstart link using base Playwright code. ${chalk.yellow(
-        "Uncomment line 118 in index.ts to have Stagehand take over!"
-      )}`
-    );
-  } catch (e) {
-    if (!(e instanceof Error)) {
-      throw e;
-    }
-    announce(
-      `${chalk.red(
-        "Looks like an error occurred running Playwright. Let's have Stagehand take over!"
-      )} \n${chalk.gray(e.message)}`,
-      "Playwright"
-    );
-
-    const actResult = await page.act({
-      action: "Click the link to the quickstart",
+  async function actWithCache(instruction: string) {
+    // Observe the page and return the action to execute
+    const results = await page.observe({
+      instruction,
+      onlyVisible: false, // Faster/better/cheaper, but uses Chrome a11y tree so may not always target directly visible elements
+      returnAction: true, // return the action to execute
     });
-    announce(
-      `${chalk.green(
-        "Clicked the quickstart link using Stagehand AI fallback."
-      )} \n${chalk.gray(actResult)}`,
-      "Act"
-    );
+    console.log(chalk.blue("Got results:"), results);
+
+    // You can cache the playwright action to use it later with no additional LLM calls :)
+    const actionToCache = results[0];
+    console.log(chalk.blue("Taking cacheable action:"), actionToCache);
+
+    // OPTIONAL: Draw an overlay over the relevant xpaths
+    await drawObserveOverlay(page, results);
+    await page.waitForTimeout(1000); // Can delete this line, just a pause to see the overlay
+    await clearOverlays(page);
+
+    // Execute the action
+    await page.act(actionToCache);
   }
 
-  //   Close the browser
-  await stagehand.close();
+  await page.goto("https://docs.stagehand.dev/reference/introduction");
 
-  console.log(
-    [
-      "To recap, here are the steps we took:",
-      `1. We went to ${chalk.blue("https://docs.browserbase.com/")}`,
-      `---`,
-      `2. We used ${chalk.green(
-        "extract"
-      )} to find information about the quickstart`,
-      `The ${chalk.bgYellow(description.title)} is at: ${chalk.bgYellow(
-        chalk.blue(description.link)
-      )}` +
-        `\n\n${chalk.bgYellow(description.description)}` +
-        `\n\n${chalk.gray(JSON.stringify(description, null, 2))}`,
-      `---`,
-      `3. We used ${chalk.green(
-        "observe"
-      )} to find the links under the 'Guides' section and got the following results:`,
-      `\nWe could have clicked:\n${observeResult
-        .map(
-          (r) => `"${chalk.yellow(r.description)}" -> ${chalk.gray(r.selector)}`
-        )
-        .join("\n")}`,
-      `---`,
-      `4. We used Playwright to click the first link. If it failed, we used ${chalk.green(
-        "act"
-      )} to gracefully fallback to Stagehand AI.`,
-    ].join("\n\n")
+  // You can pass a string directly to act with something like:
+  // await page.act("Click the search box")
+  // However, it's faster/cheaper/more reliable to use a cache-able approach
+  await actWithCache("Click the search box");
+
+  await actWithCache(
+    "Type 'Tell me in one sentence why I should use Stagehand' into the search box"
   );
+  await actWithCache("Click the suggestion to use AI");
+  await page.waitForTimeout(2000);
+  const { text } = await page.extract({
+    instruction:
+      "extract the text of the AI suggestion from the search results",
+    schema: z.object({
+      text: z.string(),
+    }),
+    useTextExtract: false, // Set this to true if you want to extract longer paragraphs
+  });
+  console.log(chalk.green("AI suggestion:"), text);
 }
 
-function announce(message: string, title?: string) {
-  console.log(
-    boxen(message, {
-      padding: 1,
-      margin: 3,
-      title: title || "Stagehand",
-    })
-  );
-}
